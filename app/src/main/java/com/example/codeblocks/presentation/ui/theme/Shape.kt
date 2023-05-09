43ee44ac4a0453566997d67548e8ad8bd316cab0
Lvntt <daymatt@mail.ru>
2023-05-09 11:56:50 +0700

    Занес строки в strings, размерности в Dimens, форму в Shape, добавил placeholder у TextField

---
+++ app/src/main/java/com/example/codeblocks/presentation/ui/theme/Shape.kt
@@ -0,0 +1,5 @@
+package com.example.codeblocks.presentation.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+
+val BlockElementShape = RoundedCornerShape(25)
--- app/src/main/java/com/example/codeblocks/presentation/ui/view/VariableDeclarationBlock.kt
+++ app/src/main/java/com/example/codeblocks/presentation/ui/view/VariableDeclarationBlock.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -25,11 +24,23 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
+import com.example.codeblocks.R
 import com.example.codeblocks.presentation.ui.theme.BlockAccentedTextStyle
+import com.example.codeblocks.presentation.ui.theme.BlockElementShape
+import com.example.codeblocks.presentation.ui.theme.BlockHeight
+import com.example.codeblocks.presentation.ui.theme.BlockMinimumWidth
+import com.example.codeblocks.presentation.ui.theme.BlockPadding
 import com.example.codeblocks.presentation.ui.theme.BlockRegularTextStyle
+import com.example.codeblocks.presentation.ui.theme.InnerBlockElementStartPadding
+import com.example.codeblocks.presentation.ui.theme.TextFieldHeight
+import com.example.codeblocks.presentation.ui.theme.TextFieldMinimumWidth
+import com.example.codeblocks.presentation.ui.theme.TextFieldPadding
+import com.example.codeblocks.presentation.ui.theme.VariableTypeChoiceHeight
+import com.example.codeblocks.presentation.ui.theme.VariableTypeChoiceWidth
 
 @Composable
 fun VariableDeclarationBlock(
@@ -42,11 +53,11 @@ fun VariableDeclarationBlock(
 
     Box(
         modifier = modifier
-            .height(80.dp)
-            .widthIn(350.dp, Dp.Infinity)
-            .clip(RoundedCornerShape(15.dp))
+            .height(BlockHeight)
+            .widthIn(BlockMinimumWidth, Dp.Infinity)
+            .clip(BlockElementShape)
             .background(MaterialTheme.colorScheme.primaryContainer)
-            .padding(16.dp)
+            .padding(BlockPadding)
     ) {
         Row(
             modifier = modifier.fillMaxSize(),
@@ -58,17 +69,17 @@ fun VariableDeclarationBlock(
                 contentAlignment = Alignment.Center
             ) {
                 Text(
-                    text = "Create",
+                    text = stringResource(id = R.string.create),
                     color = MaterialTheme.colorScheme.onPrimaryContainer,
                     style = BlockRegularTextStyle
                 )
             }
             Box(
                 modifier = modifier
-                    .padding(start = 12.dp)
-                    .height(48.dp)
-                    .width(80.dp)
-                    .clip(RoundedCornerShape(15.dp))
+                    .padding(start = InnerBlockElementStartPadding)
+                    .height(VariableTypeChoiceHeight)
+                    .width(VariableTypeChoiceWidth)
+                    .clip(BlockElementShape)
                     .background(MaterialTheme.colorScheme.background),
                 contentAlignment = Alignment.Center
             ) {
@@ -104,23 +115,23 @@ fun VariableDeclarationBlock(
                 }
             }
             Box(
-                modifier = modifier.padding(start = 12.dp),
+                modifier = modifier.padding(start = InnerBlockElementStartPadding),
                 contentAlignment = Alignment.Center
             ) {
                 Text(
-                    text = "variable",
+                    text = stringResource(id = R.string.variable),
                     color = MaterialTheme.colorScheme.onPrimaryContainer,
                     style = BlockRegularTextStyle
                 )
             }
             Box(
                 modifier = modifier
-                    .padding(start = 12.dp)
+                    .padding(start = InnerBlockElementStartPadding)
                     .width(IntrinsicSize.Min)
-                    .height(48.dp)
-                    .clip(RoundedCornerShape(15.dp))
+                    .height(TextFieldHeight)
+                    .clip(BlockElementShape)
                     .background(MaterialTheme.colorScheme.background)
-                    .padding(horizontal = 8.dp),
+                    .padding(horizontal = TextFieldPadding),
                 contentAlignment = Alignment.Center
             ) {
                 BasicTextField(
@@ -128,9 +139,19 @@ fun VariableDeclarationBlock(
                     onValueChange = {
                         textFieldContent = it
                     },
-                    modifier = modifier.widthIn(100.dp, Dp.Infinity),
+                    modifier = modifier.widthIn(TextFieldMinimumWidth, Dp.Infinity),
                     singleLine = true,
-                    textStyle = BlockAccentedTextStyle.copy(textAlign = TextAlign.Center)
+                    textStyle = BlockAccentedTextStyle.copy(textAlign = TextAlign.Center),
+                    decorationBox = { innerTextField ->
+                        if (textFieldContent.isEmpty()) {
+                            Text(
+                                text = stringResource(id = R.string.namePlaceholder),
+                                color = Color.LightGray,
+                                style = BlockRegularTextStyle.copy(textAlign = TextAlign.Center)
+                            )
+                        }
+                        innerTextField()
+                    }
                 )
             }
         }
