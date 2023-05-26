dd94a755ebf3b4f24b06ee2f5a0b75385f2eb7cd
Lvntt <daymatt@mail.ru>
2023-05-27 02:29:57 +0700

    Уменьшил размер блоков

--- app/src/main/java/com/example/codeblocks/ui/theme/TextStyles.kt
+++ app/src/main/java/com/example/codeblocks/ui/theme/TextStyles.kt
@@ -5,13 +5,13 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val BlockRegularTextStyle = TextStyle(
-    fontSize = 18.sp,
+    fontSize = 17.sp,
     fontFamily = Oxygen,
     fontWeight = FontWeight.Normal
 )
 
 val BlockAccentedTextStyle = TextStyle(
-    fontSize = 18.sp,
+    fontSize = 17.sp,
     fontFamily = Oxygen,
     fontWeight = FontWeight.Bold
 )
